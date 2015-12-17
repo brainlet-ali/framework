brainlet-ali/framework

commit 800e8b62f6ec1571036846f5e864d3e4cae6add5

Clean up unneeded line.

src/Illuminate/Auth/RequestGuard.php
@@ -51,9 +51,7 @@ class RequestGuard implements Guard
             return $this->user;
         }
 
-        $user = call_user_func($this->callback, $this->request);
-
-        return $this->user = $user;
+        $this->user = call_user_func($this->callback, $this->request);
     }
 
     /**

commit 6855cbbf4175f475fe9d23607d1a20e3d616246a

Fix RequestGuard::user() returning void
Broken since 800e8b62f6ec1571036846f5e864d3e4cae6add5

src/Illuminate/Auth/RequestGuard.php
@@ -51,7 +51,7 @@ class RequestGuard implements Guard
             return $this->user;
         }
 
-        $this->user = call_user_func($this->callback, $this->request);
+        return $this->user = call_user_func($this->callback, $this->request);
     }
 
     /**